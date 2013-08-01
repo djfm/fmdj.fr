b422e2a24617e5054c96e2b3f56df4abcddbed57
Bob <bob@fmdj.fr>
2013-08-01 18:50:59 +0000

    added ip verification for deploy and test content page

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+/*********
+* CONFIG *
+**********/
+
+var allowed_to_deploy = ['204.232.175.64/27', '192.30.252.0/22'];
+
+
+/*********
+*  CODE  *
+**********/
+
 var express    = require('express'),
 	sys        = require('sys'),
 	spawn      = require('child_process').spawn,
 	fs		   = require('fs'),
-	winston    = require('winston');
+	winston    = require('winston'),
+	ip		   = require('./ip.js');
 
 var app     = express();
 
@@ -31,14 +43,36 @@ process.on('SIGINT', function() {
 	});
 });
 
-app.get('/deploy', function(req, res){
-	info.log("Got order to deploy, fingers crossed!");
-	res.send();
-	var child = spawn("./deploy.sh", [], {detached: true, stdio:['ignore', 'ignore', 'ignore']});
+app.post('/deploy', function(req, res){
+
+	var from_ip = req.headers['x-forwarded-for'].split(',')[0].trim();
+
+	var ok = false;
+	for(var i in allowed_to_deploy)
+	{
+		if(ip.inSubNet(from_ip, allowed_to_deploy[i]))
+		{
+			ok=true;
+			break;
+		}
+	}
+
+	if(ok)
+	{
+		info.log("Got order to deploy, fingers crossed!");
+		res.send();
+		var child = spawn("./deploy.sh", [], {detached: true, stdio:['ignore', 'ignore', 'ignore']});
+	}
+	else
+	{
+		info.log("Cannot deploy from " + from_ip);
+	}
+
+	
 });
 
 app.get('/', function(req, res){
-	res.send('/');
+	res.send(req.hostname);
 });
 
 app.listen(8888);
